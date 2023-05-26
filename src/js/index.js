55e48d0e113344fefc776cb66ca21dc4e0f1a603
Santana-Victor <santanavictor592@gmail.com>
2023-05-26 11:23:31 -0300

    1ª commit - upload de todo o projeto

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+const abrirMenu = document.getElementById("botao-menu");
+
+function mudancaMenu(event) {
+    if (event.type === "touchstart") event.preventDefault();
+    const menu = document.getElementById("nav");
+    menu.classList.toggle("aberto")
+};
+
+abrirMenu.addEventListener("click", mudancaMenu);
+abrirMenu.addEventListener("touchstart", mudancaMenu);
